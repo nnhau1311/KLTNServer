30e563815da656e58b5edeebdd4b29ea1fc47623
Thai Anh Hao <thaianhhao322@gmail.com>
2022-11-23 00:15:31 +0700

    sửa config cors

--- src/main/java/com/example/childrenhabitsserver/config/SimpleCORSFilter.java
+++ src/main/java/com/example/childrenhabitsserver/config/SimpleCORSFilter.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class SimpleCORSFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
+//        Filter.super.init(filterConfig);
     }
 
     @Override
@@ -19,11 +19,11 @@ public class SimpleCORSFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         chain.doFilter(req, res);
     }
